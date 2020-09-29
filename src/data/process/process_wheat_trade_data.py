d9239871c328d6c322733d9011dd87d31443c26d
Martin Jensen <albergren@users.noreply.github.com>
2020-09-29 23:53:13 +0200

    Select yield data and interpolate

--- src/data/process/process_wheat_trade_data.py
+++ src/data/process/process_wheat_trade_data.py
@@ -4,10 +4,12 @@ import pandas as pd
 import os
 import sys
 sys.path.append('grain-price-data/')
+sys.path.append('tools/')
 import datasources as ds
-
+import DatasetBuilder as db
 IMPORT_FLOW_CODE = 1
 EXPORT_FLOW_CODE = 2
+YIELD_STRUCPRO_CODE = 'YI'
 
 partners_to_drop = ['EU_EXTRA','EU_INTRA','WORLD','EA19_EXTRA','EA19_INTRA',
                     'EU27_2020_EXTRA','EU27_2020_INTRA','EU28_EXTRA','EU28_INTRA',
@@ -20,6 +22,14 @@ years_to_drop = list(map(str, range(int(ds.wheat_trade_data['start_year']),
 path_to_importExport_data = '/home/martin/gp/grain-price-forcasting/src/data/process/test/importExport_data/'
 path_to_production_data = '/home/martin/gp/grain-price-forcasting/src/data/process/test/production_data/'
 
+def calculate_feature(data):
+    feature = []
+    print(data)
+    for index, row in data.iterrows():
+        print(row)
+    return feature
+
+features = []
 for filename in os.listdir(path_to_importExport_data):
     df = pd.read_csv(path_to_importExport_data + filename)
     df = df[df['value'].notna()]
@@ -30,9 +40,6 @@ for filename in os.listdir(path_to_importExport_data):
                                 + reporter
                                 + ds.wheat_production_data["filename"][1])
                                 
-    df_production = pd.read_csv(path_to_production_data + production_data_filename)
-
-    
     # Drop partners that are aggregate and rows with yearly sums
     # and set value column to numeric
     df = df[~df['PARTNER'].isin(partners_to_drop)]
@@ -49,7 +56,19 @@ for filename in os.listdir(path_to_importExport_data):
     # Group periods
     df_import = df_import.groupby('TIME_PERIOD')['value'].sum() 
     df_export = df_export.groupby('TIME_PERIOD')['value'].sum() 
+
+    df_production = pd.read_csv(path_to_production_data + production_data_filename)
+    df_production = df_production[df_production['STRUCPRO'] == YIELD_STRUCPRO_CODE]
+
+    df_production = db.DatasetBuilder(df_production)
+    df_production.resample_year_to_month('TIME_PERIOD')
     
-    #print(df_import)
+    df_production = df_production.get_set()
+    df = pd.DataFrame({'yield':df_production['value'], 'import': df_import, 'export': df_export})
+    
+    feature = calculate_feature(df)  
+    features.append(feature)
     
 
+df_features = pd.concat(features,axis=1)
+print(df_features)
